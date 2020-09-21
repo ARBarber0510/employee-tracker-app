3415e99e50f0ac3b82b910c69938da49be204cf0
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-09-21 12:54:34 -0500

    linked js file to mysql, inquirer, and console.table

---
+++ employee-tracker.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var consoleTable = require("console.table");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Sea91Mar0510!",
+    database: ""
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+
+});
